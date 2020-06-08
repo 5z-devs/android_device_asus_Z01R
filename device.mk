fca65645db847020cef212936794c5f2c27f3d8f
Jackeagle <jackeagle102@gmail.com>
2020-06-08 11:39:18 +0200

    Z01R: Import seccomp media policy
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: Ia6c58067f02e72e60147ccaa608af975160fecee

--- device.mk
+++ device.mk
@@ -294,6 +294,10 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
