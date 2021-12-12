4e69dcf90d472dc328dd80fd77c572dcb8d9de3e
Bruno Martins <bgcngm@gmail.com>
2021-12-12 22:06:02 +0000

    [DNM] Z01R: Temp bringup
    Change-Id: Ia77eded89f56f7ff8c813ee344b71d0dcfd1403f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,6 +17,9 @@ BOARD_VENDOR := asus
 
 DEVICE_PATH := device/asus/Z01R
 
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_ENFORCE_SYSPROP_OWNER := true
+
 TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
 
 # Assert
@@ -52,6 +55,7 @@ BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 loop.max_part=7
 BOARD_KERNEL_CMDLINE += buildv=WW_100.10.107.123
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
