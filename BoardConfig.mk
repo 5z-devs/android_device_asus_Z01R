cf671c85f0772b0a6560dad9dcb575fbb209e563
Chris Fries <cfries@google.com>
2022-08-02 15:58:22 +0530

    Z01R: Set printk.devkmsg=on on kernel cmdline to avoid ratelimit
    init and friends are getting ratelimted on /dev/kmsg.  Let's keep
    the logs by setting printk.devkmsg=on.
    
    Bug: 62952768
    Test: Check for init logs in kernel during boot
    Change-Id: I103e3cc1d73880af1d9a0cdc4dae05223eb4c427

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,6 +49,7 @@ BOARD_KERNEL_CMDLINE := \
     loop.max_part=7 \
     lpm_levels.sleep_disabled=1 \
     msm_rtb.filter=0x237 \
+    printk.devkmsg=on \
     service_locator.enable=1 \
     swiotlb=2048 \
     video=vfb:640x400,bpp=32,memsize=3072000
