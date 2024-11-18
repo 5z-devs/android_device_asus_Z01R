7544f2bcaedb9f0210f752a4070acf2464951a2f
Marc Bourgoin <themard85@gmail.com>
2024-11-17 21:22:53 -0700

    Z01R: Disable ELF checks on some blobs
    Change-Id: Id1bd5f001665b188d8b61c10e7aee6bf80ff791f

--- extract-files.py
+++ extract-files.py
@@ -43,6 +43,7 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.imsrtpservice@3.0',
     ): lib_fixup_vendor_suffix,
     (
+        'libOmxCore',
         'libqsap_sdk',
         'libril',
         'libwpa_client',
